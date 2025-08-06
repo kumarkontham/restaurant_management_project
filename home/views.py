dd7e8b2866c68c38b6fa051a3c53c2c41ab3fc9e
kumarkontham <kumarkontham1@gmail.com>
2025-08-06 22:41:06 +0530

    24102

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+@api_view(['GET'])
+def home_page(request):
+    return render(request,'home/home.html')
 # Create your views here.
