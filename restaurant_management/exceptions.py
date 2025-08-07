ca28a5aeeed0438b57280c4f1a98e9e3c524dfa8
kumarkontham <kumarkontham1@gmail.com>
2025-08-07 22:16:01 +0530

    24103

--- restaurant_management/exceptions.py
+++ restaurant_management/exceptions.py
@@ -7,5 +7,4 @@ def custom_exception_handler(exc,context):
     response= exception_handler(exc,context)
     if response is None and isinstance(exc,Http404):
         return Response({"error":"The request source not found","status":404},status=status.Http_404_NOT_FOUND)
-
     return response
