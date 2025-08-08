d444f6691fbad6d645fa4558a39e50c1b096d30e
kumarkontham <kumarkontham1@gmail.com>
2025-08-08 22:42:32 +0530

    24105

--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView 
 from .models import Restaurant
 from rest_framework.response import Response
 from django.views.decorators.http import require_GET
-class RestaurantNameAPI(APIView):
-    def get(self,request):
-        restaurant = Restaurant.objects.first()
-        name = restaurant.name if restaurant else settings.RESTAURANT_NAME
-        return Response({"name":name})
+@api_view(["GET"])
+def get(self,request):
+    restaurant = Restaurant.objects.first()
+    name = restaurant.name if restaurant else settings.RESTAURANT_NAME
+    return Response({"name":name})
 @require_GET
 def home_page(request):
     restaurant = Restaurant.objects.first()
