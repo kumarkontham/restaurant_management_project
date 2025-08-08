20f2ae42406f94b0ceccec6fcd0c65d5b4e698bc
kumarkontham <kumarkontham1@gmail.com>
2025-08-08 23:20:17 +0530

    24106

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_page,name="home_page")
+    path('',views.home_page,name="home_page"),
+    path('about/',views.about_us,name="about_us")
 ]
