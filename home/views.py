7c6b8dd0cef3883051281b196218bc18d7979638
kumarkontham <kumarkontham1@gmail.com>
2025-08-08 22:30:12 +0530

    24105

--- home/views.py
+++ home/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView 
 from .models import Restaurant
+from rest_framework.response import Response
 @api_view(['GET'])
 def home_page(request):
     return render(request,'home/home.html')
-# Create your views here.
+class RestaurantNameAPI(APIView):
+    def get(self,request):
+        restaurant = Restaurant.objects.first()
+        name = restaurant.name if restaurant else settings.RESTAURANT_NAME
+        return Response({"name":name})
+def homepage(request):
+    restaurant = Restaurant.objects.first()
+    restaurant = restaurant.name if restaurant else settings.RESTAURANT_NAME
+    return render(request,'home/home.html',{'restaurant_name':name})
+    
