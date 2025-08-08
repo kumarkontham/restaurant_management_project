7d3c43c7ed820355a4dc7ca85adc1ed69c9abcf1
kumarkontham <kumarkontham1@gmail.com>
2025-08-08 22:22:05 +0530

    24105

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView 
+from .models import Restaurant
 @api_view(['GET'])
 def home_page(request):
     return render(request,'home/home.html')
