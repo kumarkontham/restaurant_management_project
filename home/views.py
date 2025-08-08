32caef0d83967544077ed07d788854080cfd50da
kumarkontham <kumarkontham1@gmail.com>
2025-08-08 22:35:33 +0530

    24105

--- home/views.py
+++ home/views.py
@@ -3,15 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView 
 from .models import Restaurant
 from rest_framework.response import Response
-@api_view(['GET'])
-def home_page(request):
-    return render(request,'home/home.html')
+from django.views.decorators.http import require_GET
 class RestaurantNameAPI(APIView):
     def get(self,request):
         restaurant = Restaurant.objects.first()
         name = restaurant.name if restaurant else settings.RESTAURANT_NAME
         return Response({"name":name})
-def homepage(request):
+@require_GET
+def home_page(request):
     restaurant = Restaurant.objects.first()
     restaurant = restaurant.name if restaurant else settings.RESTAURANT_NAME
     return render(request,'home/home.html',{'restaurant_name':name})
