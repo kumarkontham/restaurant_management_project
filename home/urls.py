0ad50277e3517522e873e223c44a9d8d0d7394f2
kumarkontham <kumarkontham1@gmail.com>
2025-08-09 20:44:07 +0530

    27618

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home_page,name="home_page"),
-    path('about/',views.about_us,name='about_us')
+    path('about/',views.about_us,name='about_us'),
+    path('menu/',views.menu_items_view,name='menu'),
 ]
