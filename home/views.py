f4065758ad775c9c3ea377f277243649f8bad4c6
kumarkontham <kumarkontham1@gmail.com>
2025-08-09 21:45:37 +0530

    27618

--- home/views.py
+++ home/views.py
@@ -24,8 +24,10 @@ def about_us(request):
     "with ingredients from local farms.Our goal is to provide healthy and tasty food "
     return render(request,"home/about.html",{"restaurant_name":name})
 def menu_items(request):
-    menu_items=[{"id":1,"name":"Home","url":'/'},
-    {"id":2,"name":"AboutUs","url":"/about/"}]
+    menu_items=[{"id":1,"Starters":"Home","url":'#'},
+    {"id":2,"mname":"Mains","url":"#"},
+    {"id":3,"mname":"Desserts","url":"#"},
+    {"id":4,"mname":"Drinks","url":"#"}]
     return render(request,'home/menu.html',{"menu_items":menu_items})
     
 
