d87914a9f760d28f8be53a28f76166c4302e31b2
kumarkontham <kumarkontham1@gmail.com>
2025-08-10 23:14:32 +0530

    27620

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view
 from .models import Restaurant,MenuItems
 def home_page(request):
     """display restaurantname on the home page
-    fetch from the database if available otherwise get the name from settings file """
+    fetch from the database if available otherwise get the name from settings file"""
     restaurant = Restaurant.objects.first()
     restaurant_name = restaurant.name if restaurant else getattr(settings,"RESTAURANT_NAME","culture food")
     return render(request,'home/home.html',{"restaurant_name":restaurant_name})
