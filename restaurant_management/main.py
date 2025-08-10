f660abd0455a6da3eb4236f1ea4410958978c057
kumarkontham <kumarkontham1@gmail.com>
2025-08-10 23:09:59 +0530

    27620

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -1,5 +1,5 @@
 def display_success_message():
-    return "basic css added successfully!."
+    return "basic css added successfully!"
     
 if __name__ == "__main__":
-    display_success_message()
+    print(display_success_message())
