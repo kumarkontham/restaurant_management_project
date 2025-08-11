156016241a9f81655fed19502b11b44bb837e3df
kumarkontham <kumarkontham1@gmail.com>
2025-08-11 21:16:36 +0530

    27624

--- home/models.py
+++ home/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+
     def __str__(self):
         return self.name
-class MenuItems(models.Model):
-    menu_name = models.CharField(max_length=100)
-    url = models.CharField(max_length=200)
-    def __str__(self):
-        return self.menu_name
