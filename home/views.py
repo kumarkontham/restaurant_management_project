9dc2c841eb6e4e064dbea55f3a66626ffcb1db0a
kumarkontham <kumarkontham1@gmail.com>
2025-08-11 20:23:02 +0530

    27621

--- home/views.py
+++ home/views.py
@@ -1,23 +1,27 @@
 from django.conf import settings
-from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Restaurant,MenuItems
+from .models import Restaurant
 def home_page(request):
     """display restaurantname on the home page
-    fetch from the database if available otherwise get the name from settings file
-    after accessing the name this view rendering to the home.html template to show the data in staic pages."""
+        fetch from the database if available otherwise get the name from settings file """
     restaurant = Restaurant.objects.first()
     restaurant_name = restaurant.name if restaurant else getattr(settings,"RESTAURANT_NAME","culture food")
     return render(request,'home/home.html',{"restaurant_name":restaurant_name})
-def get_restaurant_name(request):
+@api_view(["GET"])
+def get_restaurant_details(request):
     """display the restaurant name in Json format"""
     restaurant = Restaurant.objects.first()
     name = restaurant.name if restaurant else getattr(settings,"RESTAURANT_NAME","culture food")
-    return JsonResponse("restaurant_name":name)
-
-    
-
+    return Response({"name":name})
+def get_restaurant_name():
+    return getattr(settings,"RESTAURANT_NAME","culture food")
+def about_us(request):
+    name=get_restaurant_name()
+    description=f"welcome to {name} restaurant!"
+    "Here we are providing service for delicious and freshly prepared meals"
+    "with ingredients from local farms.Our goal is to provide healthy and tasty food "
+    return render(request,"home/about.html",{"restaurant_name":name})
 
     
