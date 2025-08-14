4dd5ea714039a2d21345cb09d35e556af878de29
kumarkontham <kumarkontham1@gmail.com>
2025-08-14 16:30:40 +0530

    32344

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-def footer_tag(notice:str="All Rights Reserved!."):
+def generate_footer(notice:str = "All Rights Reserved!."):
     curr_year = datetime.now().year 
-    return f"&copy; {curr_year}|{notice}"
-
-place_holder = 'reservation page is under construction!. Reservation features available soon.'
-def reservation_message():
-    return place_holder
+    return f'&copy; {curr_year}|{notice}'
+def reservation_message(place_holder:str="reservation page is under construction!. Reservation features available soon."):
+    return f'{place_holder}'
 def display_success_message():
     return "basic css styles added  successfully for the home page"
     
