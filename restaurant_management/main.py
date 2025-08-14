b512c519f193c00c3f3fdc646090aaac434603ce
kumarkontham <kumarkontham1@gmail.com>
2025-08-14 16:45:20 +0530

    32344

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -6,10 +6,9 @@ def reservation_message(place_holder:str="reservation page is under construction
     return f'{place_holder}'
 def display_success_message():
     return "basic css styles added  successfully for the home page"
-if __name__ == "__main__":
-    print(display_success_message())
-def get_contact():
+def retrive_contact():
     return "+91-8976543245"
 if __name__ == "__main__":
-    print(get_contact())
+    print(display_success_message())
+
 
