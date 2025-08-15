11c3ba417ea9f74892643040c6a588f27ac6b0ab
kumarkontham <kumarkontham1@gmail.com>
2025-08-15 21:03:20 +0530

    32346

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
+    logo = models.ImageField(upload_to='logos/',blank=True,null=True)
 
     def __str__(self):
         return self.name
