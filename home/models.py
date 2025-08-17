ce7e6c305325b5c753651a84b809f81b0dfe5e97
kumarkontham <kumarkontham1@gmail.com>
2025-08-17 15:29:37 +0530

    33693

--- home/models.py
+++ home/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=15)
-    logo = models.ImageField(upload_to='logos/',blank=True,null=True)
-
     def __str__(self):
         return self.name
+class MenuItems(models.Model):
+    menu_name = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    def __str__(self):
+        return self.menu_name
+class Feedback(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.id}-{self.cooment}"
