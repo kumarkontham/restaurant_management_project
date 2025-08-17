06058a281f09cb6056b9f1a96778575f3466895f
kumarkontham <kumarkontham1@gmail.com>
2025-08-17 15:48:43 +0530

    33693

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.home_page,name="home_page"),
-    path('about/',views.about_us,name="about_us"),
-    path('contactus/',views.Contact_us,name="contact_us"),
-    path('menu/',views.Menu_items,name="menu"),
-    path('reservation/',views.reservation_view,name='reservation_view'),
-    path('errors/',views.error_handling,name="error_handling"),
+    path('about/',views.about_us,name='about_us'),
+    path('menu/',views.hardcoded_items,name='menu'),
+    path('feedback/',views.Feedback_view,name="feedback"),
+    path('fedback/completed/',views.Feed)
 ]
