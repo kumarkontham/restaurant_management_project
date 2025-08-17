8ff520b6b6ae6249beb0bac46493d8b6ba34d7f4
kumarkontham <kumarkontham1@gmail.com>
2025-08-17 15:49:30 +0530

    33693

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.home_page,name="home_page"),
     path('about/',views.about_us,name='about_us'),
     path('menu/',views.hardcoded_items,name='menu'),
-    path('feedback/',views.Feedback_view,name="feedback"),
-    path('fedback/completed/',views.Feed)
+    path('feedback/',views.feedback_view,name="feedback"),
+    path('fedback/completed/',views.feedback_thanks_view,name="feedback_completed")
 ]
