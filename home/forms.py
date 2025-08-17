35d641e9981b7f802fcd95fb0dc6001afd49d678
kumarkontham <kumarkontham1@gmail.com>
2025-08-17 15:27:03 +0530

    33693

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-class ContactForm(forms.Form):
+class ContactForm(forms.ModelForm):
     name = forms.CharField(max_length=100,required=True,label="your name")
     email = forms.EmailField(required=True,label="Your email")
     message = forms.CharField(width=forms.Textarea,required = True, label ="Your message")
+class FeedbackForm(forms.ModelForm):
+    
