fdb962cef77cb932a05c898c321143721f488c7c
kumarkontham <kumarkontham1@gmail.com>
2025-08-22 21:24:45 +0530

    37295

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -44,6 +44,17 @@ class Feedback(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.id}-{self.comment}"
+#models.py
+class Order(models.Model):
+    STATUS_CHOICES =[(STATUS_PENDING,"pending"),
+    (STATUS_IN_PROGRESS,"in_progress"),
+    (STATUS_COMPLETED,"completed"),
+    (STATUS_CANCELED,"cancled"),]
+    customer = models.ForeignKey(User,on_delete=CASCADE,related_name='orders')
+    total_amount = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default="pending")
+    def __str__(self):
+        return f"order {self.id} by {self.customer}"
 #forms.py
 class FeedbackForm(forms.ModelForm):
     class Meta:
