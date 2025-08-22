0c78a8abcf9e1aa850b028ff36fa5769df434903
kumarkontham <kumarkontham1@gmail.com>
2025-08-22 21:48:05 +0530

    37295

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu_items,Orders
+admin.site.register(Menu_items)
+admin.site.register(Orders)
 # Register your models here.
