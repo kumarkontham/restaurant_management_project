ed0e56d8b080e7de83a485105f43ec88b0d083eb
kumarkontham <kumarkontham1@gmail.com>
2025-08-23 19:59:22 +0530

    37292

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from django import forms
+from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
 from django.urls import path
 from rest_framework.views import APIView
@@ -67,6 +68,12 @@ class Menu_items(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
     def __str__(self):
         return self.name
+class User_profile(models.Model):
+    user = models.OneToOneField(User,on_delete=CASCADE)
+    email = models.EmailField(unique=True)
+    Mobile_number = models.CharField(max_length=15,blank=True)
+    def __str__(self):
+        return self.user.username
 #forms.py
 class FeedbackForm(forms.ModelForm):
     class Meta:
