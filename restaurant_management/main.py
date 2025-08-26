8eed2a797e6744e99850c21b1f28db3546911367
kumarkontham <kumarkontham1@gmail.com>
2025-08-26 15:28:28 +0530

    39266

--- restaurant_management/main.py
+++ restaurant_management/main.py
@@ -140,8 +140,6 @@ urlpatterns =[
     path("",views.home_view,name = "home_page"),
     path("menu/",views.menu_view,name="menu_items"),
     # path("feedback/",views.feedback_view,name="feedback_form"),
-    # path("menu/",views.MenuAPIView.as_view(),name="menu_items"),
-    # path("menu_items/",Menu_itemsAPI_view.as_view(),name="menu_api"),
 ]
 def display_success_message():
     return "basic css styles added  successfully for the home page"  
