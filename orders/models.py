50d3e7d61fab60757a658fb3a34911ac439913d4
kumarkontham <kumarkontham1@gmail.com>
2025-09-17 21:44:02 +0530

    47166

--- orders/models.py
+++ orders/models.py
@@ -5,3 +5,7 @@ class OrderStatus(models.Model):
     name = models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name
+class Order(models.Model):
+    status = models.ForeignKeyField('OrderStatus',on_delete = models.SET_Null,null=True,blank=True,related_name='orders')
+    def __str__(self):
+        return f"{self.pk} {self.status}"
