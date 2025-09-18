57aa0027dcd071331ea23d1532ff0dd135e288cd
kumarkontham <kumarkontham1@gmail.com>
2025-09-18 22:13:41 +0530

    47169

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers 
 from .models import MenuCategory
 
-class MenuCategorySerializers(serializers.ModelSerializer):
+class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ["name"]
